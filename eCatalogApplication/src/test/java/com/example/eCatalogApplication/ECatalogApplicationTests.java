4547861a93ad302da43c6e8546a3d0cc8d039bd3
Iulian Florea <iulian.florea4@gmail.com>
2023-08-27 11:55:26 +0300

    1

---
+++ eCatalogApplication/src/test/java/com/example/eCatalogApplication/ECatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eCatalogApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
